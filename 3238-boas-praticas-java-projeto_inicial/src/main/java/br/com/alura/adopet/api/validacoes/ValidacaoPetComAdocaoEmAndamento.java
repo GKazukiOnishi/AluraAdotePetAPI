606eebefe70ed3c432b7a19b6a7cb4d0da2d41b9
GKazukiOnishi <kazuki.onishi@hotmail.com>
2024-02-10 17:05:19 -0300

    Refactor - Removendo redundâncias e utilizando convenções JPA - Melhorando a performance de queries nas validações de adoções

--- 3238-boas-praticas-java-projeto_inicial/src/main/java/br/com/alura/adopet/api/validacoes/ValidacaoPetComAdocaoEmAndamento.java
+++ 3238-boas-praticas-java-projeto_inicial/src/main/java/br/com/alura/adopet/api/validacoes/ValidacaoPetComAdocaoEmAndamento.java
@@ -2,16 +2,11 @@ package br.com.alura.adopet.api.validacoes;
 
 import br.com.alura.adopet.api.dto.SolicitacaoAdocaoDTO;
 import br.com.alura.adopet.api.exception.ValidacaoException;
-import br.com.alura.adopet.api.model.Adocao;
-import br.com.alura.adopet.api.model.Pet;
 import br.com.alura.adopet.api.model.StatusAdocao;
 import br.com.alura.adopet.api.repository.AdocaoRepository;
-import br.com.alura.adopet.api.repository.PetRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
-
-import java.util.List;
 
 @Component
 @Order(2)
@@ -19,17 +14,12 @@ public class ValidacaoPetComAdocaoEmAndamento implements ValidacaoSolicitacaoAdo
 
     @Autowired
     private AdocaoRepository repository;
-    @Autowired
-    private PetRepository petRepository;
 
     @Override
     public void validar(SolicitacaoAdocaoDTO dto) {
-        Pet pet = petRepository.getReferenceById(dto.idPet());
-        List<Adocao> adocoes = repository.findAll();
-        for (Adocao a : adocoes) {
-            if (a.getPet() == pet && a.getStatus() == StatusAdocao.AGUARDANDO_AVALIACAO) {
-                throw new ValidacaoException("Pet já possui outra adoção aguardando avaliação!");
-            }
+        boolean petTemAdocaoEmAndamento = repository.existsByPetIdAndStatus(dto.idPet(), StatusAdocao.AGUARDANDO_AVALIACAO);
+        if (petTemAdocaoEmAndamento) {
+            throw new ValidacaoException("Pet já possui outra adoção aguardando avaliação!");
         }
     }
 }
